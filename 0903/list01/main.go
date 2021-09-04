d2e7d21f32627814a4695e74eec48c92665d4dd0
Jungheon-Oh <zsnd.oj@gmail.com>
2021-09-04 23:35:01 +0900

    data structure

--- 0903/list01/main.go
+++ 0903/list01/main.go
@@ -6,13 +6,27 @@ import (
 )
 
 func main() {
-	v := list.New()
-	e4 := v.PushBack(4)
-	e1 := v.PushFront(1)
-	v.InsertBefore(3, e4)
-	v.InsertAfter(2, e1)
+	v := list.New() //새로운 리스트 생성
+	fmt.Println(v.Len())
 
-	for e := v.Front(); e != nil; e = e.Next() {
+	e4 := v.PushBack(4) //리스트 뒤에 요소 추가 [4]
+	fmt.Println(v.Len())
+
+	e1 := v.PushFront(1) //리스트 앞에 요소 추가 [1][4]
+	fmt.Println(v.Len())
+
+	v.InsertBefore(3, e4) //e4 요소 앞에 요소 삽입 [1][3][4]
+	fmt.Println(v.Len())
+
+	v.InsertAfter(2, e1) //e4 요소 뒤에 요소 삽입
+	fmt.Println(v.Len())
+
+	for e := v.Front(); e != nil; e = e.Next() { //초기문 e := v.Front() 실행 -->  조건문 e != nil 검사 --> 함수 본문 실행 --> 후처리 e = e.Next() 실행 --> 조건 --> 본문 --> 후처리 반복
+		fmt.Print(e.Value, "")
+	}
+
+	fmt.Println()
+	for e := v.Back(); e != nil; e = e.Prev() {
 		fmt.Print(e.Value, "")
 	}
 }
---
+++ 0904/ring/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"container/ring"
+	"fmt"
+)
+
+func main() {
+	r := ring.New(5)
+
+	n := r.Len()
+
+	for i := 0; i < n; i++ {
+		r.Value = 'A' + i
+		fmt.Printf("%c ", r.Value)
+		fmt.Println()
+		r = r.Next()
+		fmt.Printf("%c ", r.Value)
+	}
+
+	for j := 0; j < n; j++ {
+		fmt.Printf("%c ", r.Value)
+		r = r.Next()
+	}
+
+	fmt.Println()
+
+	for j := 0; j < n; j++ {
+		fmt.Printf("%c ", r.Value)
+		r = r.Prev()
+	}
+}
