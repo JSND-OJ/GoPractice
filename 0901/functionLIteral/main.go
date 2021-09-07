098a0572c0db500fd8485011cb2c5ea20b1250a3
Jungheon-Oh <zsnd.oj@gmail.com>
2021-09-07 19:09:24 +0900

    .

--- 0901/functionLIteral/main.go
+++ 0901/functionLIteral/main.go
@@ -7,11 +7,11 @@ type opFunc func(a, b int) int
 func getOperator(op string) opFunc {
 	if op == "+" {
 		return func(a, b int) int {
-			return a + b
+			return a + b //함수 리터럴을 사용해서 더하기 함수르 정의하고 반환
 		}
 	} else if op == "*" {
 		return func(a, b int) int {
-			return a * b
+			return a * b //함수 리터럴을 사용해서 곱하기 함수르 정의하고 반환
 		}
 	} else {
 		return nil
@@ -21,6 +21,6 @@ func getOperator(op string) opFunc {
 func main() {
 	fn := getOperator("*")
 
-	result := fn(3, 4)
+	result := fn(3, 4) //함수 타입 변수를 사용해서 함수 호출
 	fmt.Println(result)
 }
