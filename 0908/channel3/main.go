93e0b67c17823c3fdbdc59ff9b9b54d44fa6bff5
Jungheon-Oh <zsnd.oj@gmail.com>
2021-09-11 22:50:58 +0900

    Practice

--- 0908/channel3/main.go
+++ 0908/channel3/main.go
@@ -7,11 +7,11 @@ import (
 )
 
 func square(wg *sync.WaitGroup, ch chan int) {
-	for n := range ch {
+	for n := range ch { //2.데이터를 계속 기다림 ->for문이 끝나서 더이상 채널이 들어오지 않지만 그래도 계속 기다림.
 		fmt.Printf("Square: %d\n", n*n)
 		time.Sleep(time.Second)
 	}
-	wg.Done()
+	wg.Done() // . 실행되지 않음
 }
 
 func main() {
@@ -22,7 +22,8 @@ func main() {
 	go square(&wg, ch)
 
 	for i := 0; i < 10; i++ {
-		ch <- i * 2
+		ch <- i * 2 // 1.데이터를 넣음
 	}
-	wg.Wait()
+	close(ch) //3.채널을 닫아준다.
+	//wg.Wait() // 작업 완료를 기다림. 근데 필요없지않음?
 }
