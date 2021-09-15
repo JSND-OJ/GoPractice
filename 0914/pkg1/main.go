1132f65142f8d6e9b2c14b53a1d285930c189ccd
Jungheon-Oh <zsnd.oj@gmail.com>
2021-09-15 22:14:20 +0900

    practice

--- 0914/pkg1/main.go
+++ 0914/pkg1/main.go
@@ -1,32 +1,14 @@
 package main
 
 import (
-	"bufio"
+	"GoPractice/myPKG/input"
 	"fmt"
 	"log"
-	"os"
-	"strconv"
-	"strings"
 )
-
-func getFloat() (float64, error) {
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		return 0, err
-	}
-
-	input = strings.TrimSpace(input)
-	number, err := strconv.ParseFloat(input, 64)
-	if err != nil {
-		return 0, err
-	}
-	return number, nil
-}
 
 func main() {
 	fmt.Print("Enter a temperature in Fahrenheit: ")
-	fahrenheit, err := getFloat()
+	fahrenheit, err := input.GetFloat()
 	if err != nil {
 		log.Fatal(err)
 	}
