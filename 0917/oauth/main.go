c9835f21d85c616b7629d0d093c9d7fafc413aae
Jungheon-Oh <zsnd.oj@gmail.com>
2021-09-19 07:31:16 +0900

    Oauth

--- 0917/oauth/main.go
+++ 0917/oauth/main.go
@@ -1,24 +1,57 @@
 package main
 
 import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
 	"encoding/json"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/pat"
 	"github.com/gorilla/sessions"
+	"github.com/urfave/negroni"
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
 var store = sessions.NewCookieStore([]byte("secret"))
 
-func main() {
-	http.HandleFunc("/", RenderMainView)
-	http.HandleFunc("/auth", RenderAuthView)
-	http.HandleFunc("/auth/callback", Authenticate)
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
 
-	log.Fatal(http.ListenAndServe(":1333", nil))
+const (
+	CallBackURL = "http://localhost:1333/auth/callback"
+
+	UserInfoAPIEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo"
+	ScopeEmail          = "https://www.googleapis.com/auth/userinfo.email"
+	ScopeProfile        = "https://www.googleapis.com/auth/userinfo.profile"
+)
+
+var OAuthConf *oauth2.Config
+
+func init() {
+	OAuthConf = &oauth2.Config{
+		ClientID:     "google client id",
+		ClientSecret: "google client secret",
+		RedirectURL:  CallBackURL,
+		Scopes:       []string{ScopeEmail, ScopeProfile},
+		Endpoint:     google.Endpoint,
+	}
+}
+
+func GetLoginURL(state string) string {
+	return OAuthConf.AuthCodeURL(state)
+}
+
+func RandToken() string {
+	b := make([]byte, 32)
+	rand.Read(b)
+	return base64.StdEncoding.EncodeToString(b)
 }
 
 func RenderTemplate(w http.ResponseWriter, name string, data interface{}) {
@@ -54,13 +87,13 @@ func Authenticate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	token, err := OAuthConf.Exchange(oauth2.NoContext, r.FormValue("code"))
+	token, err := OAuthConf.Exchange(context.Background(), r.FormValue("code"))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	client := OAuthConf.Client(oauth2.NoContext, token)
+	client := OAuthConf.Client(context.Background(), token)
 	userInfoResp, err := client.Get(UserInfoAPIEndpoint)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -84,4 +117,15 @@ func Authenticate(w http.ResponseWriter, r *http.Request) {
 	session.Save(r, w)
 
 	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func main() {
+	mux := pat.New()
+	mux.HandleFunc("/", RenderMainView)
+	mux.HandleFunc("/auth", RenderAuthView)
+	mux.HandleFunc("/auth/callback", Authenticate)
+
+	n := negroni.Classic()
+	n.UseHandler(mux)
+	log.Fatal(http.ListenAndServe(":3000", n))
 }
